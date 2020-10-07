b0aa6067a0bfbfd63121a81d4bcd59405f7909f3
Tajiiii <daisuke.tajima.0218@gmail.com>
2020-10-07 12:58:11 +0900

    add devise

---
+++ app/controllers/services_controller.rb
@@ -0,0 +1,10 @@
+class ServicesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'users/show'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/unsubscribe'
+  get 'users/destroy'
+  get 'services/index'
+  get 'services/show'
+  get 'services/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
